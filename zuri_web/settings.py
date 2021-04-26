c8db86ccf7317c0a80b5425b6c80c4621d1ba995
Muanya11 <nwokejichimuanya@gmail.com>
2021-04-26 23:40:35 +0100

    Add Heroku deployment files

--- zuri_web/settings.py
+++ zuri_web/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '#3qbhb7c-_ojz-y8c@ad@3k@+sh$@q#j7mp%em-+@**!&+mo!p'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['p3-blog-app.herokuapp.com']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+	'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
